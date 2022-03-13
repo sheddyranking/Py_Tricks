6093e3711477fbcd571025c6abff2bd21153d6bb
sheddyranking <shedyisaac@gmail.com>
2022-03-13 21:38:58 +0100

    Evaluate Multiple Conditions

---
+++ Eva_ Multi_Conditions.py
@@ -0,0 +1,35 @@
+# Multiple Comparisons
+
+
+# The typical way
+if a < 4 and a > 1:
+    # do something here
+
+# Do this instead
+if 1 < a < 4:
+    # do somerthing here
+
+
+#ANOTHER SCENARIO
+
+# The typical way
+if b == "Mon" or b == "Wed" or b == "Fri" or b == "Sun":
+    # do something here
+
+# Do this instead, you can also specify a tuple ("Mon", "Wed", "Fri", "Sun")
+if b in "Mon Wed Fri Sun".split():
+    # do something here
+
+
+#ANOTHER SCENARIO
+
+# The typical ways
+if a < 10 and b > 5 and c == 4:
+    # do something
+if a < 10 or b > 5 or c == 4:
+    # do something
+# Do these instead
+if all([a < 10, b > 5, c == 4]):
+    # do something
+if any([a < 10, b > 5, c == 4]):
+    # do something
