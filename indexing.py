819738475e133a171b754d5902e2908e198160c8
sheddyranking <shedyisaac@gmail.com>
2022-03-13 01:10:14 +0100

    indexing

--- indexing.py
+++ indexing.py
@@ -4,3 +4,14 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8]
 print("First Number:", numbers[0])
 print("First Four Numbers:", numbers[:4])
 print("Odd Numbers:", numbers[::2])
+
+print(' ')
+
+# Negative Indexing
+data_shape = (100, 50, 4)
+names = ["John", "Aaron", "Mike", "Danny"]
+hello = "Hello World!"
+
+print(data_shape[-1]) # 4
+print(names[-3:-1]) # ['Aaron', 'Mike']
+print(hello[1:-1:2]) # 
